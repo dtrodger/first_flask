1405dd433d52174541a0119f2379c82267df7e90
David Rodgers <dtrodger20@gmail.com>
2016-09-06 20:43:49 -0500

    adds name form to index view

--- hello.py
+++ hello.py
@@ -8,32 +8,50 @@ from flask import request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.script import Manager
+from flask.ext.wtf import Form
 
 from datetime import datetime
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fjdskfr4k353fj943jf34jrkscf9r384'
 
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
-def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', name=name, form=form, current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
 
 # @app.route('/')
 # def index():
